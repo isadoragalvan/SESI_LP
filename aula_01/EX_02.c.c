cd48120024e4d2b9b65e7033dca3f78e39fb5cdc
Isadora <isadora.galvan@aluno.senai.br>
2026-03-30 12:10:11 -0300

    aula 01 C

---
+++ aula_01/EX_02.c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    int n1;
+    
+    printf("Digite o valor de n1:");
+    scanf("%i", &n1);
+    
+    int n2;
+    printf("Digite o valor de n2:");
+    scanf("%i", &n2);
+    
+    printf ("Resultado: %i", n1+n2);
+}
